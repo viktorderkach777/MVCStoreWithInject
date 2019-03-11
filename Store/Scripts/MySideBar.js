fb8b1a94de0ceb407f1bce1918e94e8475cb100b
viktorderkach777 <viktorderkach777@yandex.ua>
2019-03-11 20:44:51 +0200

    changes on the lesson

--- Store/Scripts/MySideBar.js
+++ Store/Scripts/MySideBar.js
@@ -1,10 +1,41 @@
-﻿
+﻿(function () {
+    // Your base, I'm in it!
+    var originalAddClassMethod = jQuery.fn.toggleClass;
+
+    jQuery.fn.toggleClass = function () {
+        // Execute the original method.
+        var result = originalAddClassMethod.apply(this, arguments);
+
+        // trigger a custom event
+        jQuery(this).trigger('cssClassChanged');
+
+        // return the original result
+        return result;
+    }
+})();
+
+
+
+
     $(document).ready(function () {
         $('#sidebarCollapse').on('click', function () {
             $('#sidebar').toggleClass('active');
             $(this).toggleClass('active');
+           
+            //TouristMap.resize();
 
+            $("#sidebar").on('cssClassChanged', function () {
+                TouristMap.resize();
+            });
+
+            $(this).on('cssClassChanged', function () {
+                TouristMap.resize();
+            });
         });
+
+
+
     });
 
-    
+
+
